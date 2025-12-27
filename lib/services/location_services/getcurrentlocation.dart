430079075f399ca5381e1f250931976fbf491744
SA SHARIF KHAN <sharifbmdd@gmail.com>
2025-12-27 21:45:56 +0600

    Update

--- lib/services/location_services/getcurrentlocation.dart
+++ lib/services/location_services/getcurrentlocation.dart
@@ -5,51 +5,64 @@ import 'package:http/http.dart' as http;
 
 class LocationService {
   static Future<Map<String, dynamic>> getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+    try {
+      bool serviceEnabled;
+      LocationPermission permission;
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      throw Exception("Location services are disabled.");
-    }
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        throw Exception("Location services are disabled.");
+      }
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+      permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
-        // throw Exception("Location permission denied.");
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // throw Exception("Location permission denied.");
+          return {"place": "Unknown"};
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        // throw Exception("Location permission permanently denied.");
         return {"place": "Unknown"};
       }
-    }
 
-    if (permission == LocationPermission.deniedForever) {
-      // throw Exception("Location permission permanently denied.");
-      return {"place": "Unknown"};
-    }
+      final pos = await Geolocator.getCurrentPosition(
+        locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
+      );
 
-    final pos = await Geolocator.getCurrentPosition(
-      locationSettings: LocationSettings(accuracy: LocationAccuracy.high),
-    );
+      String placeName = "Unknown";
 
-    String placeName = "Unknown";
-
-    try{
-      var resposnse = await http.get(Uri.parse("https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${pos.latitude}&lon=${pos.longitude}&accept-language=en"),headers: {'User-Agent': 'Nectar/1.0'});
+      try {
+        var resposnse = await http.get(
+          Uri.parse(
+            "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${pos.latitude}&lon=${pos.longitude}&accept-language=en",
+          ),
+          headers: {'User-Agent': 'Nectar/1.0'},
+        );
         if (resposnse.statusCode == 200 || resposnse.statusCode == 201) {
           var jsonlocationn = jsonDecode(resposnse.body);
-          placeName = "${jsonlocationn['address']['road']}, ${jsonlocationn['address']['city']}";
+          placeName =
+              "${jsonlocationn['address']['road']}, ${jsonlocationn['address']['city']}";
         } else {
           placeName = "Unknown Location";
         }
+      } catch (e) {
+        placeName = "Unknown Location";
+      }
+
+      return {
+        "latitude": pos.latitude,
+        "longitude": pos.longitude,
+        "place": placeName,
+      };
     } catch (e) {
-      placeName = "Unknown Location";
+      return {
+        "latitude": 000,
+        "longitude": 000,
+        "place": "Rangpur",
+      };
     }
-
-
-    return {
-      "latitude": pos.latitude,
-      "longitude": pos.longitude,
-      "place": placeName,
-    };
   }
 }
